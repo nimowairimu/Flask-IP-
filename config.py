2ab61455bc4cc42712f1cfd58be42fa890e640f5
nimowairimu <nimowairimu25@gmail.com>
2021-02-25 11:34:28 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
-    NEWS_ARTICLES_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    NEWS_API_BASE_URL =os.environ.get('https://newsapi.org/v2/sources?language=en&category={}&apiKey={}')
+    NEWS_ARTICLES_URL =os.environ.get('https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}')
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
